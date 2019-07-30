fb64bfc722e5c28461d04d5becc1a5515e9c4410
Martin Nøhr Åskov Hansen <noehraaskov@gmail.com>
2019-07-30 23:47:42 +0200

    Updates

---
+++ precache-manifest.b589daeed7998eaec3d059938f0018b7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c577f1d15ba3ca200b9d3e9fd50202ad",
+    "url": "/HiddenMuseums/index.html"
+  },
+  {
+    "revision": "66ed9fad2147d0c96325",
+    "url": "/HiddenMuseums/static/css/main.ce9dda1b.chunk.css"
+  },
+  {
+    "revision": "eda75385665104c79c6c",
+    "url": "/HiddenMuseums/static/js/2.0aa6a791.chunk.js"
+  },
+  {
+    "revision": "66ed9fad2147d0c96325",
+    "url": "/HiddenMuseums/static/js/main.5a41a374.chunk.js"
+  },
+  {
+    "revision": "1d244a0056fbf336cbe0",
+    "url": "/HiddenMuseums/static/js/runtime~main.3e7f4fdd.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.516e147f8f8bb49df2cd6075b0a8e985.js"
+  "/HiddenMuseums/precache-manifest.b589daeed7998eaec3d059938f0018b7.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/HiddenMuseums/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
